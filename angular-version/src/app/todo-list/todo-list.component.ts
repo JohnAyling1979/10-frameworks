8331d358aa9eea93d848e0d4741ab7b266a29660
John Ayling <john.ayling@digintent.com>
2022-07-31 15:10:44 -0500

    unsubscribe on distroy.

--- angular-version/src/app/todo-list/todo-list.component.ts
+++ angular-version/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { TodosService } from 'src/services/todos.service';
 
 @Component({
@@ -6,13 +7,20 @@ import { TodosService } from 'src/services/todos.service';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, OnDestroy {
   todos: string[] = [];
+  todosSubscription: Subscription | null = null;
 
   constructor(private todoService: TodosService) { }
 
   ngOnInit(): void {
     this.todoService.getTodos();
-    this.todoService.todosSubjext.subscribe(todos => this.todos = todos);
+    this.todosSubscription = this.todoService.todosSubjext.subscribe(todos => this.todos = todos);
+  }
+
+  ngOnDestroy(): void {
+    if (this.todosSubscription) {
+      this.todosSubscription.unsubscribe();
+    }
   }
 }
